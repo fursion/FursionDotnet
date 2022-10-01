b58ebfb82d38b6e0cd763c8283be2169a1aff484
fursion <604357968@qq.com>
2022-10-01 14:47:19 +0800

    init

---
+++ FursionBlogCore/FursionBlogCore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Fursion.FursionBlogCore
+{
+    public class FursionBlogCore
+    {
+        public string API_Version { get; set; }
+        public FursionBlogCore()
+        {
+           
+        }
+    }
+}
+
